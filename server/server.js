b0d85c0a811ee777589a03e8bad7648825792299
nicodvarg <dv.nico13@gmail.com>
2022-11-28 13:58:10 -0300

    RestServer: Inicio

---
+++ server/server.js
@@ -0,0 +1,24 @@
+require("./config/config.js");
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(require("./routes/usuario"));
+
+mongoose.connect(process.env.URLDB, (err, res) => {
+
+    if (err) throw err;
+
+    console.log("Base de datos ONLINE");
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Escuchando puerto:", process.env.PORT);
+});
